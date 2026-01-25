bb9c4a05ba838c193455e83b7ce078903c68772b
qa-tester22 <wavenlondon@gmail.com>
2026-01-25 20:46:17 +0000

    3 homework for lecture 3

---
+++ 3_home_work_3.js
@@ -0,0 +1,17 @@
+// Лекция 3. Массивы. Связанные списки.
+
+// Домашнее задание к Лекции 3.
+// https://leetcode.com/quest/data-structures-and-algorithms-quest/
+// 3 задачи первого уровня, и 2 задачи 2 уровня 
+
+// Видео Лекции 1 на Ютубе - 
+// https://youtu.be/A5OaUtLouBk
+// Задачи к Лекции 1 - 34, 35, 704, 278, 283 Литкода
+
+// Видео Лекции 2 на Ютубе - 
+// https://www.youtube.com/watch?v=MJrw17r0aD0&list=PLAvBnOyKzDyL2xRURIi2Tpu9Z_Wrga6oi
+
+// Домашнее задание к Лекции 2.
+// https://leetcode.com/
+// 1, 46, 215, 217, 242
+
